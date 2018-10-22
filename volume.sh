076fc3f3110b18457e3e3a7dd06a714135bbf263
Penn Bauman <penn.bauman@gmail.com>
2018-10-22 13:17:52 -0400

    added i3bar conky-json volume

--- volume.sh
+++ volume.sh
@@ -1,38 +1,10 @@
 #/bin/sh
-vol="$(pactl list sinks | grep 'Volume: front-left')"
-echo $vol
-echo ${#vol}
+vol="$(pactl list sinks | grep '^[[:space:]]Volume:' | \
+    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
+
 #vol="$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }')"
-i=0
-while :; do
-    echo "i = $i : ${vol:($i):($i)}"
-    if [[ ${vol:($i):($i)} == "%" ]]; then
-        echo "found %"
-        echo ${vol:0:$i}
-        break
-    elif [[ ${vol:$i:($i+1)} == " " ]]; then
-        echo "found \" \""
-        vol=${vol:0:$i}
-        i=0
-    fi
-    if (( $i > ${#vol} )); then
-        break
-    fi
-    i=$(($i + 1))
-done
-
-#if [[ ${vol:1:2} = "%" ]]; then
-#    echo "0$vol  "
-#else
-#    echo "$vol  "
-#fi
-
-#if [[ ${vol:0:1} > 4 ]];then
-#    echo "> 50%"
-#fi
-#if [[ ${vol:0:1} < 5 ]];then
-#    echo "< 50%"
-#fi
-#if [[ ${vol:1:2} = "%" ]]; then
-#    echo "< 10%"
-#fi
+if (( $vol < 10 )); then
+    echo "0$vol"
+else
+    echo "$vol"
+fi
