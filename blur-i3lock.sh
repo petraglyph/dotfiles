9cb6c66aa413613b059508a1d570b9209fdd9722
Penn Bauman <penn.bauman@gmail.com>
2018-10-24 19:26:32 -0400

    Switche blur lock to pixelate

--- blur-i3lock.sh
+++ blur-i3lock.sh
@@ -4,8 +4,8 @@
 scrot /tmp/screen_locked.png
 
 # Pixellate it 10x
-#mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
-mogrify /tmp/screen_locked.png -blur 0x8 /tmp/screen_locked.png
+mogrify -scale 10% -scale 1000% /tmp/screen_locked.png
+#mogrify /tmp/screen_locked.png -blur 0x8 /tmp/screen_locked.png
 
 # Lock screen displaying this image.
 i3lock -i /tmp/screen_locked.png -e -u
