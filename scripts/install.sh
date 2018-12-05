bb3890a07bd6de9bd7a7fec53cb4ca1172740a6d
Penn Bauman <penn.bauman@gmail.com>
2018-12-04 22:03:00 -0500

    moved configs to folder

--- scripts/install.sh
+++ scripts/install.sh
@@ -4,22 +4,19 @@
 # REMOVE EXISTING CONFIGS
 rm ~/.config/rofi/config
 rm ~/.config/twmn/twmn.conf
-rm ~/.config/polybar/config
+
 rm ~/.config/termite/config
 rm ~/.xsession
 sudo rm /bin/i3locker.sh
 sudo rm /etc/X11/xorg.conf.d
-#sudo rm /bin/polybar-start.sh
 echo "Configs Cleared"
 # LINK NEW CONFIGS
-ln ~/.i3/rofi-config ~/.config/rofi/config
-ln ~/.i3/twmn.conf ~/.config/twmn/twmn.conf
-ln ~/.i3/polybar ~/.config/polybar/config
-ln ~/.i3/termite-config ~/.config/termite/config
-cp ~/.i3/xsession ~/.xsession
+ln ~/.i3/configs/rofi-config ~/.config/rofi/config
+ln ~/.i3/configs/twmn.conf ~/.config/twmn/twmn.conf
+ln ~/.i3/configs/termite-config ~/.config/termite/config
+cp ~/.i3/configs/xsession ~/.xsession
 sudo ln ~/.i3/scripts/i3locker.sh /bin/
 sudo ln -r -s ~/.i3/xorg.conf.d /etc/X11/
-#sudo ln ~/.i3/polybar-start.sh /bin/
 echo "Configs Linked"
 
 # PROGRAM INSTALLS
