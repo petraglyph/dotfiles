1b5c6dd3016b07718dff84c32dcb1d1cbea15528
Penn Bauman <penn.bauman@gmail.com>
2019-01-06 19:10:40 -0500

    switched volume check in config to pamixer

--- scripts/volume.sh
+++ scripts/volume.sh
@@ -1,6 +1,5 @@
 #/bin/sh
-vol="$(pactl list sinks | grep '^[[:space:]]Volume:' | \
-    head -n $(( $SINK + 1 )) | tail -n 1 | sed -e 's,.* \([0-9][0-9]*\)%.*,\1,')"
+vol="$(pamixer --get-volume)"
 
 #vol="$(amixer sget Master | grep 'Right:' | awk -F'[][]' '{ print $2 }')"
 if (( $vol < 10 )); then
