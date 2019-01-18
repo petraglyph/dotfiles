ca0c91fb79ea0949e4f443b2a0a0cc73bcd4cb81
Penn Bauman <penn.bauman@gmail.com>
2019-01-17 21:58:30 -0500

    changed center of lock to background

--- scripts/i3locker.sh
+++ scripts/i3locker.sh
@@ -15,12 +15,12 @@ totem="$(pgrep totem | wc -l)"
 # write info to lock-log.txt
 echo "Locker:  $screens $totem $(date +%Y-%m-%d_%H:%M:%S) $1" >> /home/penn/.i3/scripts/lock-log.txt
 
-i3lock -e --color=$backcolor --indicator --force-clock \
---insidevercolor=$highcolor --insidewrongcolor=$errorcolor --insidecolor=$highcolor \
---ringvercolor=$plaincolor --ringwrongcolor=$errorcolor --ringcolor=2EB398FF \
+i3lock -e --color=$backcolor --indicator --force-clock --radius=100 --ring-width=9 \
+--insidevercolor=$linecolor --insidewrongcolor=$linecolor --insidecolor=$linecolor \
+--ringvercolor=$plaincolor --ringwrongcolor=$errorcolor --ringcolor=$highcolor \
 --linecolor=$linecolor --keyhlcolor=$plaincolor --bshlcolor=$errorcolor --separatorcolor=$linecolor \
---veriftext="" --wrongtext="" --noinputtext="" --locktext="" --lockfailedtext="" \
---datestr="%a %Y-%m-%d" --time-font=Monospace --date-font=Monospace
+--veriftext="" --wrongtext="" --noinputtext="" --locktext="" --lockfailedtext="" --datestr="%a %Y-%m-%d" \
+--timecolor=$highcolor --datecolor=$plaincolor --time-font=Monospace --date-font=Monospace
 
 # Turn the screen off after a delay.
 if [[ $1 = "dark" ]]; then
