1f3e956a1c83429a2a497df343d9d33421cf12f0
Penn Bauman <penn.bauman@gmail.com>
2019-02-01 12:56:31 -0500

    added midnight commander configs

--- scripts/install.sh
+++ scripts/install.sh
@@ -1,5 +1,5 @@
 #/bin/sh
-# bash scripts/install.sh
+# bash scripts/install.sh [quick]
 
 loc="/home/penn/.i3"
 
@@ -8,17 +8,20 @@ rm ~/.config/rofi/config
 rm ~/.config/twmn/twmn.conf
 rm ~/.config/termite/config
 rm ~/.config/dunst/dunstrc
+rm ~/.config/mc/mc.keymap
 sudo rm /bin/i3locker.sh
 sudo rm /etc/X11/xorg.conf.d
 sudo rm /etc/cron.weekly/backup
 sudo rm /etc/cron.daily/backup
 sudo rm /usr/lib/systemd/system-sleep/pre-suspend.sh
+sudo rm /usr/share/mc/skins/mc-custom.ini
 echo "Configs Cleared"
 # LINK NEW CONFIGS
 ln $loc/configs/rofi-config ~/.config/rofi/config
 ln $loc/configs/twmn.conf ~/.config/twmn/twmn.conf
 ln $loc/configs/termite-config ~/.config/termite/config
 ln $loc/configs/dunstrc ~/.config/dunst/dunstrc
+ln $loc/configs/mc.keymap ~/.config/mc/mc.keymap
 sudo ln $loc/scripts/i3locker.sh /bin/
 sudo ln -r -s $loc/xorg.conf.d /etc/X11/
 sudo cp $loc/scripts/backup-weekly.sh /etc/cron.weekly/backup
@@ -27,7 +30,12 @@ sudo chmod +x /etc/cron.weekly/backup
 sudo chmod +x /etc/cron.daily/backup
 sudo cp $loc/scripts/pre-suspend.sh /usr/lib/systemd/system-sleep/
 sudo chmod +x /usr/lib/systemd/system-sleep/pre-suspend.sh
+sudo ln $loc/configs/mc-custom-skin.ini /usr/share/mc/skins/mc-custom.ini
 echo "Configs Linked"
+
+if [ $1 == "quick" ]; then 
+    exit 0
+fi
 
 # PROGRAM INSTALLS
 pgms=()
