7f0332ebd3946b4175e80aa33c29d8b35a684a35
Penn Bauman <penn.bauman@gmail.com>
2019-02-10 15:00:27 -0500

    updated i3exit with options

--- scripts/i3exit.sh
+++ scripts/i3exit.sh
@@ -1,14 +1,40 @@
 #/bin/sh
 # i3 Exit
 
-echo "Exiting i3 in 5"
-sleep 1
-echo ": 4"
-sleep 1
-echo ": 3"
-sleep 1
-echo ": 2"
-sleep 1
-echo ": 1"
-sleep 1
-i3-msg exit
+echo "i3 System"
+echo "  [s] shutdown"
+echo "  [r] reboot"
+echo "  [e] exit i3"
+echo "  [z] sleep"
+#echo "  6) hibernate"
+echo "  [l] lock i3"
+echo "  [i] restart i3"
+echo "  [c] cancel"
+while true; do 
+    read -p ": " t
+    if [ $t == "s" ]; then 
+        i3exit shutdown
+        exit 0
+    elif [ $t == "r" ]; then 
+        i3exit reboot
+        exit 0
+    elif [ $t == "e" ]; then 
+        i3-msg exit
+        exit 0
+    elif [ $t == "z" ]; then 
+        bash i3lock
+        xset dpms force off
+        i3exit suspend
+        exit 0
+    elif [ $t == "l" ]; then 
+        bash i3locker.sh dark
+        exit 0
+    elif [ $t == "i" ]; then 
+        i3-msg restart
+        exit 0
+    elif [ $t == "c" ]; then 
+        exit 0
+    else 
+        echo " Please enter a listed letter."
+    fi
+done
