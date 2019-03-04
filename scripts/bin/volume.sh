e4db9465026d3603ab3835dec85a5b8bfe782a98
Penn Bauman <penn.bauman@gmail.com>
2019-03-03 20:59:44 -0500

    moved scripts and xorg confs, switched some keyboard shortcuts

---
+++ scripts/bin/volume.sh
@@ -0,0 +1,16 @@
+#!/bin/sh
+
+vol=$(pamixer --get-volume)
+if (( $vol > 100 )); then 
+    color="DB5B5B"
+else 
+    color="2EB398"
+fi 
+if (( $vol == 0 )); then 
+    icon=""
+elif (( $vol < 50 )); then 
+    icon=""
+else 
+    icon=""
+fi
+echo $(printf '%%{F#%s}%s%%{F}%02d%%' $color $icon $vol)
