dcd14fb82f7da50f6b14c8c7e79cf32c2f0feea0
Penn Bauman <penn.bauman@gmail.com>
2019-03-04 02:53:30 -0500

    fixed formating issues in weather scripts

--- scripts/bin/wunderground.sh
+++ scripts/bin/wunderground.sh
@@ -2,22 +2,21 @@
 # Wunderground format
 
 icons="
-sunny:
-clear:
-mostlysunny:
-partlycloudy:
-partlysunny:
-mostlycloudy:
-cloudy:
-
-flurries:
-fog:
-hazy:
-rain:
-sleet:
-snow:
-tstorms:
-unknown:稜
+:sunny:
+:clear:
+:mostlysunny:
+:partlycloudy:
+:partlysunny:
+:mostlycloudy:
+:cloudy:
+:flurries:
+:fog:
+:hazy:
+:rain:
+:sleet:
+:snow:
+:tstorms:
+:unknown:稜
 "
 
 zip="22904"
@@ -25,7 +24,7 @@ apikey=$(cat $HOME/.api/wunderground)
 curl "http://api.wunderground.com/api/$apikey/conditions/q/$zip.json" -o $HOME/.bin/weather.json 2> /dev/null
 weather="$HOME/.bin/weather.json"
 
-temp=$(jq .current_observation.temp_f $weather)
+temp=$(printf '%.0f' $(jq .current_observation.temp_f $weather))
 condition=$(jq .current_observation.icon $weather | cut -d'"' -f 2)
-icon=$(echo "$icons" | grep "$condition" | cut -d":" -f 2)
+icon=$(echo "$icons" | grep ":$condition:" | cut -d":" -f 3)
 echo "%{F#2EB398}$icon%{F} $temp°F"
