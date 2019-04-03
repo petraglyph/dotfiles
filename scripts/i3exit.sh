1b47f1180dbf17a12a7a56f961ef1684ef9cf267
Penn Bauman <penn.bauman@gmail.com>
2019-04-03 00:23:40 -0400

    added update check on shutdown

--- scripts/i3exit.sh
+++ scripts/i3exit.sh
@@ -1,6 +1,21 @@
 #/bin/sh
 # i3 Exit
 
+function pacmaner {
+	if [ ! -z $(yay -Qu) ]; then
+		echo "$(yay -Qu | wc -l) packages ready to update"
+		while true; do
+			read -p "Would you like to update? [y/n] " $answer
+			case $answer in
+				y|Y) yay 
+					break;;
+				n|N) break;; 
+				*) echo 'Please enter "y" or "n"';;
+			esac
+		done
+	fi
+}
+
 echo "i3 System"
 echo "  [s] shutdown"
 echo "  [r] reboot"
@@ -12,9 +27,11 @@ echo "  [c] cancel"
 while true; do 
     read -p ": " t
     if [ $t == "s" ]; then 
+		pacmaner
         i3exit shutdown
         exit 0
     elif [ $t == "r" ]; then 
+		pacmaner
         i3exit reboot
         exit 0
     elif [ $t == "e" ]; then 
