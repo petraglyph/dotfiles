7af2ef7eb07096884ca3b4afea3ee376e8b12af8
Penn Bauman <penn.bauman@gmail.com>
2019-04-29 22:31:16 -0400

    added vim and zsh configs

--- primary/install.sh
+++ primary/install.sh
@@ -1,7 +1,7 @@
 #/bin/sh
 # bash scripts/install.sh [quick]
 
-loc="/home/penn/Storage/Linux/i3-config"
+loc="/home/penn/Storage/linux/i3-config"
 
 # REMOVE EXISTING CONFIGS
 rm $HOME/.config/i3/config
@@ -11,12 +11,16 @@ rm $HOME/.config/dunst/dunstrc
 rm $HOME/.config/mc/mc.keymap
 rm $HOME/.config/ncmpcpp/config
 rm $HOME/.config/polybar/config
+rm $HOME/.zshrc
+rm $HOME/.vimrc
+rm -r $HOME/.vim
 
 sudo rm -rf /etc/X11/xorg.conf.d
 sudo rm /etc/cron.weekly/backup
 sudo rm /etc/cron.daily/backup
 sudo rm /usr/share/mc/skins/mc-custom.ini
 echo "Configs Cleared"
+
 # LINK NEW CONFIGS
 ln -s $loc/primary/config $HOME/.config/i3/config
 ln -s $loc/configs/rofi-config $HOME/.config/rofi/config
@@ -25,6 +29,9 @@ ln -s $loc/configs/dunstrc $HOME/.config/dunst/dunstrc
 ln -s $loc/configs/mc.keymap $HOME/.config/mc/mc.keymap
 ln -s $loc/configs/ncmpcpp-config $HOME/.config/ncmpcpp/config
 ln -s $loc/configs/polybar $HOME/.config/polybar/config
+ln -s $loc/configs/zshrc $HOME/.zshrc
+ln -s $loc/configs/vimrc $HOME/.vimrc
+ln -s $loc/configs/vim $HOME/.vim
 
 sudo cp -r $loc/configs/xorg.conf.d /etc/X11/
 sudo cp $loc/scripts/backup-weekly.sh /etc/cron.weekly/backup
@@ -34,13 +41,13 @@ sudo chmod +x /etc/cron.daily/backup
 sudo cp $loc/configs/mc-custom-skin.ini /usr/share/mc/skins/mc-custom.ini
 echo "Configs Linked"
 
-if [ -d $HOME/.bin ]; then 
-    rm -f $HOME/.bin
-fi
+# CREATE ~/.bin
+rm -f $HOME/.bin
 ln -s $loc/scripts/bin $HOME/.bin
 chmod 755 ~/.bin/*
 echo "~/.bin/ created"
 
+# EDIT TERMITE CTK CSS
 if [ $(cat "$HOME/.config/gtk-3.0/gtk.css" | grep ".termite" | wc -l) == 0 ]; then 
     echo ".termite {" >> $HOME/.config/gtk-3.0/gtk.css
     echo "    padding: 8px;" >> $HOME/.config/gtk-3.0/gtk.css
@@ -50,7 +57,7 @@ else
     echo "Termite already styled in gtk.css"
 fi
 
-if [[ $1 == "quick" ]]; then 
+if [[ $1 != "pgms" ]]; then 
     exit 0
 fi
 
