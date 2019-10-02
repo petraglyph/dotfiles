637c41b69be076ede4cb5d192c1de950f4b291fb
Penn Bauman <pennbauman@protonmail.com>
2019-10-02 13:09:47 -0400

    Revert "archived backup scripts"
    This reverts commit aea6ee1e09606d775ee5967e980461f46c6aace2.

---
+++ scripts/backup-weekly.sh
@@ -0,0 +1,10 @@
+#/bin/sh
+# Backup Weekly
+# $ sudo cp ~/.i3/scripts/backup-weekly.sh /etc/cron.weekly/backup
+# $ sudo chmod +x /etc/cron.weekly/backup
+
+# rsync -a --delete --quiet /folder/to/backup /location/of/backup
+
+sudo rsync -aAXv --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/swapfile","/home/*"} / /home/penn/Storage/System-Backups/root/
+#sudo rsync -aAXv --exclude={'/home/*/.atom/*', '/Android/*', '/Applications/*', '/Desktop/*', '/Documents/*', '/Downloads/*', '/Dropbox/*', '/Music/*', '/pCloudDrive/*', '/Pictures/*', '/Public/*', '/Storage/*', '/Templates/*', '/Videos/*', '/.cache/*', '/.local/share/Steam/*'} /home/penn/ /home/penn/Storage/Backups/home/
+#rsync -a --delete --quiet /home/penn/.config/ /home/penn/Storage/Backups/config
