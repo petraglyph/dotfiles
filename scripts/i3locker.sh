8489be7cfe37c521b6d77e6b3082777426915ff2
Penn Bauman <pennbauman@protonmail.com>
2020-05-14 04:06:05 -0400

    updated configs for black background

--- scripts/i3locker.sh
+++ scripts/i3locker.sh
@@ -1,19 +1,12 @@
-#!/bin/sh -e
+#/bin/sh -e
 # $ bash ~/.i3/scripts/i3locker.#!/bin/sh
     
 ## Color i3lock
-backcolor="141A1B"
+backcolor="000000FF"
 plaincolor="EEEEEEFF"
 highcolor="2EB398FF"
 errorcolor="DB5B5BFF"
-linecolor="141A1BFF"
-
-# connected monitors
-#screens="$(xrandr | grep " connected" | wc -l)"
-# video player running
-#totem="$(pgrep totem | wc -l)"
-# write info to lock-log.txt
-#echo "Locker:  $screens $totem $(date +%Y-%m-%d_%H:%M:%S) $1" >> /home/penn/.i3/scripts/lock-log.txt
+linecolor="000000FF"
 
 i3lock -e --color=$backcolor --indicator --force-clock --radius=100 --ring-width=9 \
 --insidevercolor=$linecolor --insidewrongcolor=$linecolor --insidecolor=$linecolor \
@@ -24,7 +17,9 @@ i3lock -e --color=$backcolor --indicator --force-clock --radius=100 --ring-width
 
 # Turn the screen off after a delay.
 if [[ $1 = "dark" ]]; then
-    sleep 5; pgrep i3lock && xset dpms force off
+    sleep 5
+	pgrep i3lock && systemctl suspend
+	#xset dpms force off
 fi
 
 
