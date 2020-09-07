dd64a856351c29813fd839e269fe4f8502a636fd
Penn Bauman <pennbauman@protonmail.com>
2020-09-07 18:01:35 -0400

    removed redundant zsh setup

--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -22,10 +22,6 @@ zsh
 message "Installing Packages"
 sudo apt -y install $packages
 
-message "Setting Up ZSH"
-sudo chsh -s /usr/bin/zsh root
-chsh -s /usr/bin/zsh
-
 message "Installing Sass (from npm)"
 sudo npm install -g npm
 sudo npm install -g sass
