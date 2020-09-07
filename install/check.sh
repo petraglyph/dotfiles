a6ca8b8c43ad03f70c385934179e8d9da626f5dc
Penn Bauman <pennbauman@protonmail.com>
2020-09-07 01:08:35 -0400

    added colors to errors

--- install/check.sh
+++ install/check.sh
@@ -3,26 +3,29 @@
 message() {
 	echo -e "\033[1;32m$1\033[0m"
 }
+error() {
+	echo -e "\033[31m$1\033[0m"
+}
 
 # Check dotfiles location and comp
 if [[ "$(cd "$(dirname "$BASH_SOURCE")"; pwd)/$(basename "$BASH_SOURCE")" != \
 		"$HOME/.dotfiles/install/check.sh" ]]; then
-	echo "Move repository to ~/.dotfiles before configuring"
+	error "Move repository to ~/.dotfiles before configuring"
 	exit 1
 fi
 
 # Check computer is provided
 if (( $# == 0 )) || [[ $1 == "" ]]; then
-	echo "Computer required"
+	error "Computer required"
 	exit 1
 fi
 
 # Check computer is valid or 'none'
 if [ -d $loc/$comp ]; then
 	if [[ $(hostname) != $comp ]]; then
-		echo "Wrong computer, should be $(hostname)"
+		error "Wrong computer, should be '$(hostname)'"
 		while true; do
-			read -p "Continue anyway [y/n]: " input
+			read -p "  Continue anyway [y/n]: " input
 			case $input in
 				y|Y) break;;
 				n|N) exit 0
@@ -34,6 +37,6 @@ if [ -d $loc/$comp ]; then
 elif [[ $1 == "none" ]]; then
 	continue
 else
-	echo "Unknown computer '$comp'"
+	error "Unknown computer '$comp'"
 	exit 1
 fi
