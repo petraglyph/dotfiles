15003b1dd75d031ab8a3933558dc484843074a1b
Penn Bauman <pennbauman@protonmail.com>
2020-10-13 03:37:40 -0400

    added directories to specialist user

--- user/configure.sh
+++ user/configure.sh
@@ -34,12 +34,11 @@ mkdir -p $XDG_DATA_HOME/nvim/site/autoload/airline/themes
 ln -fs $loc/user/$user/gitconfig $XDG_CONFIG_HOME/git/config
 ln -fs $loc/configs/nvim.init.vim $XDG_CONFIG_HOME/nvim/init.vim
 ln -fs $loc/configs/ranger $XDG_CONFIG_HOME/ranger
-#ln -fs $loc/configs/user-dirs.dirs $XDG_CONFIG_HOME/user-dirs.dirs
+ln -fs $loc/configs/user-dirs.dirs $XDG_CONFIG_HOME/user-dirs.dirs
 ln -fs $loc/configs/coc-settings.json $XDG_CONFIG_HOME/nvim/coc-settings.json
 ln -fs $loc/configs/profile $HOME/.profile
 ln -fs $loc/user/$user/zshrc $HOME/.zshrc
-#ln -fs $loc/$comp/zsh-dirs $HOME/.zsh-dirs
-echo "" > $HOME/.zsh-dirs
+ln -fs $loc/user/$user/zsh-dirs $HOME/.zsh-dirs
 ln -fs $loc/configs/maia-custom.vim $XDG_DATA_HOME/nvim/site/colors/maia-custom.vim
 ln -fs $loc/configs/airline-maia_custom.vim $XDG_DATA_HOME/nvim/site/autoload/airline/themes/maia_custom.vim
 
@@ -47,3 +46,7 @@ ln -fs $loc/configs/airline-maia_custom.vim $XDG_DATA_HOME/nvim/site/autoload/ai
 if [ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ]; then
 	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 fi
+
+# Adding directories
+mkdir $HOME/college
+mkdir $HOME/downloads
