e79258ce86f035a574cf1f12b9655bedbeda2c2f
Penn Bauman <pennbauman@protonmail.com>
2021-04-03 19:53:58 -0400

    added i3 setup for hps2017

--- hps2017-ubuntu/setup.sh
+++ hps2017-ubuntu/setup.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 # Setup hps2017
 #   Ubuntu
+#   i3
 
 loc="$HOME/.dotfiles"
 comp="hps2017-ubuntu"
@@ -10,9 +11,13 @@ source "$(dirname $BASH_SOURCE)/../install/check.sh" "$comp"
 
 # Ubuntu Installs
 bash ~/.dotfiles/install/ubuntu.sh
+# i3 Fedora Installs
+bash ~/.dotfiles/i3/ubuntu-install.sh
 # Terminal Configuration
 bash ~/.dotfiles/install/terminal.sh $comp
 # Desktop Configuration
 bash ~/.dotfiles/install/desktop.sh $comp
+# i3 Configuration
+bash ~/.dotfiles/i3/configure.sh $comp
 # Install Personal Programs
 bash ~/.dotfiles/install/personal.sh
