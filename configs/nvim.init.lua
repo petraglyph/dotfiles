e229278deec34e67965ab2f86311d6555ec9fff2
Penn Bauman <me@pennbauman.com>
2022-09-27 00:38:25 -0400

    converted neovim config to lua

---
+++ configs/nvim.init.lua
@@ -0,0 +1,131 @@
+-- NeoVim Config
+--   Penn Bauman <me@pennbauman.com>
+
+-- General
+vim.opt.filetype = 'plugin'
+vim.opt.encoding = 'utf8'
+vim.opt.clipboard = 'unnamedplus'
+-- Appearance
+vim.opt.number = true
+vim.opt.linebreak = true
+vim.opt.tabstop = 4
+vim.opt.softtabstop = 4
+vim.opt.shiftwidth = 4
+vim.opt.listchars = 'tab:¦ ,trail:~,extends:>,precedes:<'
+vim.opt.list = true
+vim.opt.guicursor = ''
+vim.opt.scrolloff = 5
+vim.cmd('colorscheme maia-custom')
+-- Netrw
+vim.api.nvim_set_var('netrw_banner', 0)
+vim.api.nvim_set_var('netrw_liststyle', 3)
+
+local augroup = vim.api.nvim_create_augroup('custom', {clear = true})
+-- Set spellcheck
+local setspell = function()
+	vim.opt_local.spell = true
+end
+vim.api.nvim_create_autocmd('FileType', {pattern = 'text', group = augroup, callback = setspell})
+vim.api.nvim_create_autocmd('FileType', {pattern = 'markdown', group = augroup, callback = setspell})
+vim.api.nvim_create_autocmd('FileType', {pattern = 'tex', group = augroup, callback = setspell})
+-- Remember cursor position
+vim.api.nvim_create_autocmd('BufReadPost', {pattern = '*', group = augroup,
+	command = 'if line("\'\\"") > 1 && line("\'\\"") <= line("$") | exe "normal! g`\\"" | endif'
+})
+
+-- Keyboard Shortcuts
+vim.keymap.set('n', '<tab>', ':bnext<Enter>', {noremap = true, silent = true})
+vim.keymap.set('n', '<S-tab>', ':bprev<Enter>', {noremap = true, silent = true})
+-- Document navigation
+vim.keymap.set('', 'k', 'gk', {noremap = true, silent = true})
+vim.keymap.set('', 'j', 'gj', {noremap = true, silent = true})
+vim.keymap.set('', '<S-h>', 'g^', {noremap = true, silent = true})
+vim.keymap.set('', '<S-j>', '15gj', {noremap = true, silent = true})
+vim.keymap.set('', '<S-k>', '15gk', {noremap = true, silent = true})
+vim.keymap.set('', '<S-l>', 'g$', {noremap = true, silent = true})
+vim.keymap.set('n', '<S-u>', ':redo<Enter>', {noremap = true, silent = true})
+-- Editing
+vim.keymap.set('n', '<enter>', 'o<esc>', {noremap = true, silent = true})
+vim.keymap.set('n', ';', ':', {noremap = true, silent = true})
+vim.keymap.set('n', '<', 'v<', {noremap = true, silent = true})
+vim.keymap.set('n', '>', 'v>', {noremap = true, silent = true})
+-- Terminal
+vim.keymap.set('n', '<C-t>', ':terminal<Enter>i<end>', {noremap = true, silent = true})
+vim.cmd('autocmd TermOpen * setlocal nonumber')
+
+
+-- Plugins (packer.nvim)
+local install_path = vim.fn.stdpath('data')..'/site/pack/packer/start/packer.nvim'
+if vim.fn.empty(vim.fn.glob(install_path)) > 0 then
+	packer_bootstrap = vim.fn.system({'git', 'clone', '--depth', '1', 'https://github.com/wbthomason/packer.nvim', install_path})
+	vim.cmd [[packadd packer.nvim]]
+end
+require('packer').startup(function(use)
+	use 'wbthomason/packer.nvim'
+	use 'https://github.com/vim-airline/vim-airline.git'
+	use 'https://github.com/Lokaltog/neoranger'
+	use "terrortylor/nvim-comment"
+	use {'lervag/vimtex', ft = {'tex'}}
+	use 'nvim-lua/completion-nvim'
+	use {'steelsojka/completion-buffers', requires = {'nvim-lua/completion-nvim'}}
+
+	-- Automatically set up your configuration after cloning packer.nvim
+	if packer_bootstrap then
+		require('packer').sync()
+	end
+end)
+
+-- vim-airline
+vim.api.nvim_set_var('airline_theme', 'maia_custom')
+vim.api.nvim_set_var('airline#extensions#tabline#enabled', 1)
+
+-- neoranger
+vim.api.nvim_set_var('neoranger_viewmode', 'miller')
+vim.keymap.set('n', '<C-r>', ':RangerCurrentFile<Enter>', {noremap = true, silent = true})
+
+-- nvim-comment
+if pcall(require, 'nvim_comment') then
+	require('nvim_comment').setup({hook = function()
+		local filetype = vim.api.nvim_buf_get_option(0, "filetype")
+		if filetype == "cpp" or filetype == "c" then
+			vim.api.nvim_buf_set_option(0, "commentstring", "// %s")
+		end
+	end})
+	vim.keymap.set('n', 'c', ':CommentToggle<Enter>', {noremap = true, silent = true})
+	vim.keymap.set('v', 'c', ':CommentToggle<Enter>', {noremap = true, silent = true})
+end
+
+-- completion-nvim
+if pcall(require, 'completion') then
+	vim.opt.completeopt = 'menuone,noinsert,noselect'
+	table.insert(vim.opt.shortmess, 'c')
+	vim.api.nvim_create_autocmd('FileType', {pattern = '*', group = augroup, callback = function()
+		require('completion').on_attach({
+			sorting = 'length',
+			matching_strategy_list = {'exact', 'substring', 'fuzzy'},
+			chain_complete_list = {
+				default = {{complete_items = {'lsp', 'path', 'buffers'}}},
+				string = {{complete_items = {'path', 'buffers'}}},
+				comment = {{complete_items = {'buffers'}}},
+			},
+		})
+	end})
+	-- Use <Tab> and <S-Tab> to navigate through popup menu
+	vim.keymap.set('i', '<Tab>', function()
+		return vim.fn.pumvisible() == 1 and '<C-n>' or '<Tab>'
+	end, { expr = true })
+	vim.keymap.set('i', '<S-Tab>', function()
+		return vim.fn.pumvisible() == 1 and '<C-p>' or '<S-Tab>'
+	end, { expr = true })
+end
+
+-- VimTeX
+vim.api.nvim_create_autocmd('FileType', {pattern = 'tex', group = augroup, callback = function()
+	vim.api.nvim_set_var('tex_flavor', 'latex')
+	vim.api.nvim_set_var('vimtex_view_method', 'zathura')
+	vim.api.nvim_set_var('vimtex_quickfix_mode', 0)
+	vim.keymap.set('n', '<C-l>', ':VimtexCompile<Enter>', {noremap = true, silent = true})
+	vim.api.nvim_create_user_command('TexWC', function()
+		print(vim.api.nvim_exec(':w !detex | wc -w', true))
+	end, {bang = true})
+end})
--- install/terminal.sh
+++ install/terminal.sh
@@ -27,23 +27,20 @@ mkdir -p $XDG_DATA_HOME/nvim/site/autoload/airline/themes
 message "Linking Terminal Configs"
 ln -fs $loc/configs/gitconfig $XDG_CONFIG_HOME/git/config
 echo "*~\n*.swp" > $XDG_CONFIG_HOME/git/ignore
-ln -fs $loc/configs/nvim.init.vim $XDG_CONFIG_HOME/nvim/init.vim
 ln -fs $loc/configs/ranger $XDG_CONFIG_HOME/ranger
 ln -fs $loc/configs/profile $HOME/.profile
 ln -fs $loc/configs/aliasrc $HOME/.aliasrc
 ln -fs $loc/configs/bashrc $HOME/.bashrc
 ln -fs $loc/configs/zshrc $HOME/.zshrc
+ln -fs $loc/configs/vimrc $HOME/.vimrc
+ln -fs $loc/configs/nvim.init.lua $XDG_CONFIG_HOME/nvim/init.lua
 ln -fs $loc/configs/maia-custom.vim $XDG_DATA_HOME/nvim/site/colors/maia-custom.vim
 ln -fs $loc/configs/airline-maia_custom.vim $XDG_DATA_HOME/nvim/site/autoload/airline/themes/maia_custom.vim
 
-# Install neovim plugins
-if [ ! -f $HOME/.local/share/nvim/site/autoload/plug.vim ]; then
-	curl -fLo ~/.local/share/nvim/site/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-fi
-nvim +PlugInstall +qall
-
 message "Setting Up Zsh"
-chsh -s /usr/bin/zsh
+if [ "$(basename $SHELL)" != "zsh" ]; then
+	chsh -s /usr/bin/zsh
+fi
 if [ -f $loc/$comp/zsh-dirs ]; then
 	ln -fs $loc/$comp/zsh-dirs $HOME/.zsh-dirs
 fi
