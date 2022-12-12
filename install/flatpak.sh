03ce884c5706342348d76c4712875a086435162d
Penn Bauman <me@pennbauman.com>
2022-12-12 02:51:33 -0500

    tweaked flatpak and ubuntu installs

--- install/flatpak.sh
+++ install/flatpak.sh
@@ -2,38 +2,12 @@
 # Flatpak Installs
 #   Penn Bauman <me@pennbauman.com>
 
-message() {
-	if [ -z $BASH_SOURCE ]; then
-		echo "\033[1;32m$1\033[0m"
-	else
-		echo -e "\033[1;32m$1\033[0m"
-	fi
-}
-
-# Check flatpak installed
-if [ -z $(command -v "flatpak") ]; then
-	question="Flatpak not found, install? [y/n]"
-	while true; do
-		read -p "$question" c
-		case $c in
-			y|Y|yes) break ;;
-			n|N|no) exit 0 ;;
-			*) echo "  Invalid input, please use 'y' or 'n'" ;;
-		esac
-		question="Install? [y/n] "
-	done
-	if [ ! -z $(command -v "dnf") ]; then
-		sudo dnf -y install flatpak
-	elif [ ! -z $(command -v "apt-get") ]; then
-		sudo apt-get -y install flatpak
-	else
-		echo "Unknown package manager, could not install flatpak"
-		exit 1
-	fi
+if [ -z "$(command -v flatpak)" ]; then
+	printf "\033[1;31m%s\033[0m\n" "Flatpak not installed"
+	exit 1
 fi
 
-
-message "Installing Flatpaks"
+printf "\033[1;32m%s\033[0m\n" "[Flatpak] Installing packages"
 packages="
 com.discordapp.Discord
 com.github.finefindus.eyedropper
@@ -46,8 +20,8 @@ us.zoom.Zoom
 if [ $# -ne 0 ]; then
 	packages="$packages $@"
 fi
-flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
-flatpak remote-add --if-not-exists flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
+flatpak remote-add --if-not-exists --user flathub https://flathub.org/repo/flathub.flatpakrepo
+flatpak remote-add --if-not-exists --user flathub-beta https://flathub.org/beta-repo/flathub-beta.flatpakrepo
 if [ ! -z "$(flatpak remotes | grep fedora)" ]; then
 	flatpak -y install fedora $(echo $packages | grep -oE 'org.gnome.[a-zA-Z]*')
 	flatpak -y install flathub $(echo $packages | sed 's/org.gnome.[a-zA-Z]*//g')
@@ -57,7 +31,7 @@ fi
 flatpak -y install flathub-beta "org.gimp.GIMP"
 
 
-message "Symlinking ~/.minecraft/"
+printf "\033[1;32m%s\033[0m\n" "[Flatpak] Symlinking ~/.minecraft/"
 MC_DIR="$HOME/.var/app/com.mojang.Minecraft/.minecraft"
 mkdir -p $MC_DIR/saves
 mkdir -p $MC_DIR/resourcepacks
--- install/ubuntu.sh
+++ install/ubuntu.sh
@@ -3,10 +3,6 @@
 #   Penn Bauman <me@pennbauman.com>
 
 
-printf "\033[1;32m%s\033[0m\n" "[Ubuntu] Remove Ubuntu Advantage"
-sudo apt-get -y install ubuntu-release-upgrader-core
-sudo apt-get -y remove ubuntu-advantage-tools ubuntu-advantage-desktop-daemon
-
 printf "\033[1;32m%s\033[0m\n" "[Ubuntu] Disable snapd"
 echo "Package: snapd
 Pin: release a=*
@@ -15,5 +11,13 @@ Pin-Priority: -10" | sudo tee /etc/apt/preferences.d/nosnap.pref > /dev/null
 printf "\033[1;32m%s\033[0m\n" "[Ubuntu] Adding Neovim Stable PPA"
 sudo add-apt-repository -y ppa:neovim-ppa/stable
 
-
 $(dirname $0)/apt.sh
+
+printf "\033[1;32m%s\033[0m\n" "[Ubuntu] Remove Ubuntu Advantage"
+sudo apt-get -y install ubuntu-release-upgrader-core
+sudo apt-get -y remove ubuntu-advantage-tools ubuntu-advantage-desktop-daemon
+
+if [ "$(findmnt -no FSTYPE /)" = "btrfs" ]; then
+	printf "\033[1;32m%s\033[0m\n" "[Ubuntu] Add APT Btrfs snapshots"
+	sudo apt-get -y install apt-btrfs-snapper
+fi
