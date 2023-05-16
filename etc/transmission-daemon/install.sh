2622110c24eb43eabf1c14ff2521d694cddc9392
Penn Bauman <me@pennbauman.com>
2023-05-15 20:59:49 -0400

    changed transmission user to default

--- etc/transmission-daemon/install.sh
+++ etc/transmission-daemon/install.sh
@@ -9,6 +9,12 @@ if [ -z "$(command -v transmission-daemon)" ]; then
 fi
 
 SYSTEMD="/etc/systemd/system/transmission-daemon.service.d"
+if $(grep -qE '^debian-transmission' /etc/passwd); then
+	DAEMON_USER="debian-transmission"
+else
+	echo "Unknown transmission user"
+	exit 1
+fi
 VAR="/var/lib/transmission-daemon"
 HERE="$(dirname $(readlink -f $0))"
 
@@ -18,19 +24,18 @@ sudo systemctl stop transmission-daemon
 printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Overriding systemd service"
 sudo mkdir -p $SYSTEMD
 sudo cp $HERE/systemd-override.conf $SYSTEMD/override.conf
-sudo sed -i "s/USER/$USER/" $SYSTEMD/override.conf
 
 printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Configuring daemon"
 # Create working directories
 sudo mkdir -p $VAR/config $VAR/watch $VAR/downloads $VAR/partial
-sudo chown -R $USER:$USER $VAR
 # Configure daemon
-touch $VAR/config/settings.json
-if [ -z "$(grep -E -- '-daemon\/watch",$' "$VAR/config/settings.json")" ]; then
-	cp $HERE/settings.json $VAR/config/settings.json
+sudo touch $VAR/config/settings.json
+if [ -z "$(sudo grep -E -- '-daemon\/watch",$' "$VAR/config/settings.json")" ]; then
+	sudo cp $HERE/settings.json $VAR/config/settings.json
 	read -p "RPC Password: " input
 	sudo sed -i "s/PASSWORD/$input/" $VAR/config/settings.json
 fi
+sudo chown -R $DAEMON_USER:$DAEMON_USER $VAR
 
 printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Configuring cron backup script"
 sudo cp $HERE/backup.cron /etc/cron.daily/transmission-daemon-backup
