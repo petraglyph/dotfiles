9af32bddaaf95b24c95a68920e4de74b112bbf98
Penn Bauman <me@pennbauman.com>
2023-07-22 01:28:06 -0400

    added packaging tools install script

--- install/latex.sh
+++ install/latex.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
-# Install LaTeX tools
+# Install LaTeX Tools
 #   Penn Bauman <me@pennbauman.com>
 #   https://github.com/pennbauman/dotfiles
 
@@ -8,7 +8,8 @@ os_id="$(grep '^ID=' /etc/os-release | cut -d'=' -f 2)"
 
 if [ "$os_id" = "fedora" ]; then
 	printf "\033[1;32m%s\033[0m\n" "[LaTeX] Installing (DNF)"
-	sudo dnf -y install latexmk texlive-latex texlive-scheme-medium
+	packages="latexmk texlive-latex texlive-scheme-medium"
+	sudo dnf -y --setopt=install_weak_deps=False install $packages
 else
 	echo "Unknown OS '$os_id'"
 	exit 1
---
+++ install/packaging.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+# Install Packaging Tools
+#   Penn Bauman <me@pennbauman.com>
+#   https://github.com/pennbauman/dotfiles
+
+os_id="$(grep '^ID=' /etc/os-release | cut -d'=' -f 2)"
+
+
+if [ "$os_id" = "fedora" ]; then
+	printf "\033[1;32m%s\033[0m\n" "[Packaging] Installing (DNF)"
+	packages="fedora-packager rust-packaging"
+	sudo dnf -y --setopt=install_weak_deps=False install $packages
+elif [ "$os_id" = "debian" ]; then
+	printf "\033[1;32m%s\033[0m\n" "[Packaging] Installing (APT)"
+	packages="devscripts debhelper"
+	sudo apt-get -y --no-install---no-install-recommends install $packages
+else
+	echo "Unknown OS '$os_id'"
+	exit 1
+fi
