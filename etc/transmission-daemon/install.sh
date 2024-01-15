be6e964e86217950e30e0ad9e447bb1f404cab29
Penn Bauman <me@pennbauman.com>
2024-01-15 00:56:02 -0500

    changed transmission from system to user service

--- etc/transmission-daemon/install.sh
+++ etc/transmission-daemon/install.sh
@@ -2,45 +2,40 @@
 # Install and Setup transmission-daemon
 #   Penn Bauman <me@pennbauman.com>
 #   https://github.com/pennbauman/dotfiles
+SYSTEMD_DIR="$HOME/.config/systemd/user"
+CONFIG_DIR="$HOME/.config/transmission-daemon"
+DATA_DIR="$HOME/transmission"
+HERE="$(dirname $(realpath $0))"
 
 if [ -z "$(command -v transmission-daemon)" ]; then
 	printf "\033[1;31m%s\033[0m\n" "transmission-daemon not installed"
 	exit 1
 fi
 
-SYSTEMD="/etc/systemd/system/transmission-daemon.service.d"
-if $(grep -qE '^debian-transmission' /etc/passwd); then
-	DAEMON_USER="debian-transmission"
-else
-	echo "Unknown transmission user"
-	exit 1
+
+# Stop other transmission-daemon service
+systemctl stop --user transmission-daemon
+if [ -z "$(transmission-remote -l 2>&1 | grep "Couldn't connect")" ]; then
+	sudo systemctl --system stop transmission-daemon
+	sudo systemctl --system disable transmission-daemon
 fi
-VAR="/var/lib/transmission-daemon"
-HERE="$(dirname $(realpath $0))"
 
-# Pause service
-sudo systemctl stop transmission-daemon
+printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Adding systemd user service"
+mkdir -p $SYSTEMD_DIR
+cp $HERE/systemd.service $SYSTEMD_DIR/transmission-daemon.service
 
-printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Overriding systemd service"
-sudo mkdir -p $SYSTEMD
-sudo cp $HERE/systemd-override.conf $SYSTEMD/override.conf
-
-printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Configuring daemon"
 # Create working directories
-sudo mkdir -p $VAR/config $VAR/watch $VAR/downloads $VAR/partial
+mkdir -p $CONFIG_DIR $DATA_DIR/watch $DATA_DIR/dowloads $DATA_DIR/partial
 # Configure daemon
-sudo touch $VAR/config/settings.json
-if [ -z "$(sudo grep -E -- '-daemon\/watch",$' "$VAR/config/settings.json")" ]; then
-	sudo cp $HERE/settings.json $VAR/config/settings.json
+if [ -f $CONFIG_DIR/settings.json ]; then
+	printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Already configured"
+else
+	printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Configuring daemon"
+	cp $HERE/settings.json $CONFIG_DIR/settings.json
 	read -p "RPC Password: " input
-	sudo sed -i "s/PASSWORD/$input/" $VAR/config/settings.json
+	sed -i "s/PASSWORD/$input/" $CONFIG_DIR/settings.json
 fi
-sudo chown -R $DAEMON_USER:$DAEMON_USER $VAR
-
-printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Configuring cron backup script"
-sudo cp $HERE/backup.cron /etc/cron.daily/transmission-daemon-backup
 
 printf "\033[1;32m%s\033[0m\n" "[transmission-daemon] Starting service"
-sudo systemctl daemon-reload
-sudo systemctl enable transmission-daemon
-sudo systemctl start transmission-daemon
+systemctl enable --user transmission-daemon
+systemctl start --user transmission-daemon
