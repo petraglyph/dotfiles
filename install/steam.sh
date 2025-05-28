d4b1703e198b43a39cb18362a0d53a6a35cfa980
Penn Bauman <me@pennbauman.com>
2025-05-28 16:49:42 -0700

    switched fedora steam install to flatpak

--- install/steam.sh
+++ install/steam.sh
@@ -11,13 +11,18 @@ if [ "$os_id" = "debian" ] || [ "$os_id" = "ubuntu" ]; then
 	sudo apt-get update
 	sudo apt-get -y install steam-installer
 elif [ "$os_id" = "fedora" ]; then
-	printf "\033[1;32m%s\033[0m\n" "[Steam] Installing (DNF)"
-	if [ -z "$(dnf repolist | grep rpmfusion-nonfree)" ]; then
-		printf "\033[1;32m%s\033[0m\n" "[Steam] Enabling RPM Fusion"
-		sudo dnf -y install https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+	printf "\033[1;32m%s\033[0m\n" "[Steam] Installing (Flatpak)"
+	flatpak install -y flathub "com.valvesoftware.Steam"
+
+	if [ ! -z "$(command -v rpm-ostree)" ]; then
+		printf "\033[1;32m%s\033[0m\n" "[Steam] Installing device support (rpm-ostree)"
+		rpm-ostree install steam-devices
+	elif [ ! -z "$(command -v dnf)" ]; then
+		printf "\033[1;32m%s\033[0m\n" "[Steam] Installing device support (DNF)"
+		sudo dnf install -y steam-devices
 	fi
-	sudo dnf -y install steam
 else
-	echo "Unknown OS '$os_id'"
+	echo "Unknown OS '$os_id' try:"
+	echo "  flatpak install com.valvesoftware.Steam"
 	exit 1
 fi
